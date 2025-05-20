e24f18afbf01ee64435c241fb4eeda1443a8d8bf
Anoop Raturi <anoopraturi04@gmail.com>
2025-05-20 19:27:43 +0530

    Revert "added isAbove18"
    This reverts commit c1d8ca59ed2e52ab8a976e1c90befc8c88ebcc72.

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const age = 24;
-const name1 = 'Anoop Raturi'
+const name = 'Anoop Raturi'
 const dob = '15/08/1997'
 
 function sayName() {
-    console.log('My Name is ' + name1)
-}
-
-function isAbove18(){
-    return age >= 18
+    console.log('My Name is ' + name)
 }
