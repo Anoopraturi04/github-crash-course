c1d8ca59ed2e52ab8a976e1c90befc8c88ebcc72
Anoop Raturi <anoopraturi04@gmail.com>
2025-05-20 18:52:41 +0530

    added isAbove18

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const age = 24;
-const name = 'Anoop Raturi'
+const name1 = 'Anoop Raturi'
 const dob = '15/08/1997'
 
 function sayName() {
-    console.log('My Name is ' + name)
+    console.log('My Name is ' + name1)
+}
+
+function isAbove18(){
+    return age >= 18
 }
